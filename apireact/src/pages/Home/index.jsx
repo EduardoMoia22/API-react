346b4ff3a53c07fb199bdf123e4e9dde4c303f35
Eduardo Moia <eduardomoiasantos@gmail.com>
2022-04-26 11:50:35 -0300

    Projeto estatico finalizado

--- apireact/src/pages/Home/index.jsx
+++ apireact/src/pages/Home/index.jsx
@@ -1,7 +1,36 @@
-export default function Home(){
-    return(
-        <div>
-            
-        </div>
+import { Container, MovieList, Movie } from "./style"
+
+export default function Home() {
+
+    const movies = [
+        {
+            id: 1,
+            title: 'It: a coisa - Parte 3',
+            image_url: 'https://uploads.jovemnerd.com.br/wp-content/uploads/2017/04/poster-it-a-coisa-2-fim.jpg'
+        },
+        {
+            id: 2,
+            title: 'Spider-Man: No Way Home',
+            image_url: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/spider-mannowayhome_lob_crd_03.jpg'
+        },
+        {
+            id: 3,
+            title: 'It: a coisa - Parte 3',
+            image_url: 'https://uploads.jovemnerd.com.br/wp-content/uploads/2017/04/poster-it-a-coisa-2-fim.jpg'
+        }
+    ]
+
+    return (
+        <Container>
+            <h1>Movies</h1>
+
+            <MovieList>
+
+                {
+                    movies.map(movie => <Movie key={movie.id}> <a href="https://google.com"><img src={movie.image_url} alt="IT-2" /></a><span>{movie.title}</span></Movie>)
+                }
+                
+            </MovieList>
+        </Container>
     );
 }
